8a55badfc6d5dc943c8a227c4d87699973ea3249
Raniita <efdezsanchez@gmail.com>
2021-09-30 23:30:36 +0200

    fix: update reply_handler

--- app/utils/bot_handlers.py
+++ app/utils/bot_handlers.py
@@ -1,4 +1,7 @@
-def reply_handler(bot, update):
+from telegram import Update
+from telegram.ext import CallbackContext
+
+def reply_handler(update: Update, context: CallbackContext):
     """Reply message."""
     text = update.message.text
     update.message.reply_text(text)
